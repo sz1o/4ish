28f0b19610a0c7d60699c272bb3a353f41ddb349
sz1o <techchecker225@gmail.com>
2026-07-31 23:06:20 -0500

    Add its-raining-MENNNNNNNNNN.lua script

---
+++ its-raining-MENNNNNNNNNN.lua
@@ -0,0 +1,2 @@
+-- [ did u know i did 72 hours for these scripts, and still making more? ]
+loadstring(game:HttpGet("https://raw.githubusercontent.com/sz1o/4ish/refs/heads/main/catware-MAIN-8t34b9uf08b0293dn9038nxznr08b02-3d48b*B%24UXc98b2i.lua"))()
